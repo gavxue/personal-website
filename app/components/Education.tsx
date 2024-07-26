2bb2098aa580290805a5ff03086c359e9635b377
Gavin Xue <gavinxue050705@gmail.com>
2024-07-26 16:29:40 -0400

    Added education

--- app/components/Education.tsx
+++ app/components/Education.tsx
@@ -1,7 +1,17 @@
 export default function Education() {
     return (
-        <section className="text-center">
-            <h2>Education</h2>
+        <section className="text-center my-10">
+            <h2 className="text-4xl font-bold">Education</h2>
+            <div className="flex flex-row justify-between border rounded-lg text-left p-5 my-5">
+                <div className="">
+                    <p className="font-bold">University of Waterloo</p>
+                    <p>Candidate for the Bachelor of Computer Science</p>
+                    <p className="ml-3">&#x2022; Awarded the René Descartes National Mathematics Scholarship ($20,000)</p>
+                </div>
+                <div className="my-auto">
+                    <p>2023-2028</p>
+                </div>
+            </div>
         </section>
     )
 }
