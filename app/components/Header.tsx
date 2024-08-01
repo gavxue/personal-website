2d2c2cbe8624e5e06384adf0f4f5bd4813f7c3e6
Gavin Xue <gavinxue050705@gmail.com>
2024-08-01 10:50:02 -0400

    Added animated icons

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,17 +1,13 @@
-import { FiMail, FiLinkedin, FiGithub } from "react-icons/fi"
-
 export default function Header() {
     return (
         <header className="flex flex-col text-center my-60">
-            <h1 className="text-7xl">Gavin Xue</h1>
-            <div className="mt-10 flex justify-center gap-10 text-3xl">
-                <div className="h-7">
+            <h1 className="text-8xl">Gavin Xue</h1>
+            <div className="h-10 mt-10 flex justify-center gap-10 text-3xl">
+                <div className="h-10 flex gap-10">
                     <lottie-player src="/mail-icon.json" autoplay className="lottie-player"></lottie-player>
                     <lottie-player src="/linkedin-icon.json" autoplay className="lottie-player"></lottie-player>
+                    <lottie-player src="/github-icon.json" autoplay className="lottie-player"></lottie-player>
                 </div>
-                <FiMail />
-                <FiLinkedin />
-                <FiGithub />
             </div>
         </header >
     )
