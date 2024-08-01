8d01db099390c89e60bfbe056b3a2275b801f2f3
Gavin Xue <gavinxue050705@gmail.com>
2024-08-01 16:37:26 -0400

    Hover effects

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react"
+import { motion } from "framer-motion"
+
 export default function Header() {
     return (
         <header className="flex flex-col text-center my-60">
@@ -5,9 +8,15 @@ export default function Header() {
             <lottie-player src="/name.json" autoplay className="lottie-player"></lottie-player>
             <div className="h-10 mt-10 flex justify-center gap-10 text-3xl">
                 <div className="h-10 flex gap-20">
-                    <lottie-player src="/mail-icon.json" autoplay className="lottie-player"></lottie-player>
-                    <lottie-player src="/linkedin-icon.json" autoplay className="lottie-player"></lottie-player>
-                    <lottie-player src="/github-icon.json" autoplay className="lottie-player"></lottie-player>
+                    <a href="mailto:gavinxue050705@gmail.com" className="w-full h-full socials-icon" target="_blank" rel="noopener noreferrer">
+                        <lottie-player src="/mail-icon.json" autoplay className="socials-icon"></lottie-player>
+                    </a>
+                    <a href="https://www.linkedin.com/in/gavin-xue/" className="w-full h-full socials-icon" target="_blank" rel="noopener noreferrer">
+                        <lottie-player src="/linkedin-icon.json" autoplay className="socials-icon"></lottie-player>
+                    </a>
+                    <a href="https://github.com/gavxue" className="w-full h-full socials-icon" target="_blank" rel="noopener noreferrer">
+                        <lottie-player src="/github-icon.json" autoplay className="socials-icon"></lottie-player>
+                    </a>
                 </div>
             </div>
         </header >
