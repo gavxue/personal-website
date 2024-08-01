5eaf36139a0a476e9d1cec48d5806c03efa51681
Gavin Xue <gavinxue050705@gmail.com>
2024-08-01 09:57:10 -0400

    Added animated icons

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,11 +5,14 @@ export default function Header() {
         <header className="flex flex-col text-center my-60">
             <h1 className="text-7xl">Gavin Xue</h1>
             <div className="mt-10 flex justify-center gap-10 text-3xl">
-                <lottie-player src="/mail-icon.json" autoplay className="lottie-player"></lottie-player>
+                <div className="h-7">
+                    <lottie-player src="/mail-icon.json" autoplay className="lottie-player"></lottie-player>
+                    <lottie-player src="/linkedin-icon.json" autoplay className="lottie-player"></lottie-player>
+                </div>
                 <FiMail />
                 <FiLinkedin />
                 <FiGithub />
             </div>
-        </header>
+        </header >
     )
 }
