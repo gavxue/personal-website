f5b9e0e60905ac276087407a6638015e48bfb95a
Gavin Xue <gavinxue050705@gmail.com>
2024-08-01 11:29:07 -0400

    Added name animation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,10 @@
 export default function Header() {
     return (
         <header className="flex flex-col text-center my-60">
-            <h1 className="text-8xl">Gavin Xue</h1>
+            {/* <h1 className="text-8xl">Gavin Xue</h1> */}
+            <lottie-player src="/name.json" autoplay className="lottie-player"></lottie-player>
             <div className="h-10 mt-10 flex justify-center gap-10 text-3xl">
-                <div className="h-10 flex gap-10">
+                <div className="h-10 flex gap-20">
                     <lottie-player src="/mail-icon.json" autoplay className="lottie-player"></lottie-player>
                     <lottie-player src="/linkedin-icon.json" autoplay className="lottie-player"></lottie-player>
                     <lottie-player src="/github-icon.json" autoplay className="lottie-player"></lottie-player>
