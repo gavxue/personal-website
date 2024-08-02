55a8c2135e1e9d2feffd570d3082af09eaa83276
Gavin Xue <gavinxue050705@gmail.com>
2024-08-02 19:06:31 -0400

    Updated Lottie

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,25 +1,63 @@
+"use client";
+
 import Link from "next/link";
+import Lottie from "react-lottie-player";
+
+import name from "/public/name";
+import mailIcon from "/public/mail-icon";
+import linkedinIcon from "/public/linkedin-icon";
+import githubIcon from "/public/github-icon";
 
 export default function Header() {
-    return (
-        <header className="flex flex-col text-center my-60">
-            {/* <h1 className="text-8xl">Gavin Xue</h1> */}
-            <Link href="" className="name hvr-icon-bob hvr-icon">
-                <lottie-player src="/name.json" autoplay></lottie-player>
-            </Link>
-            <div className="h-10 mt-10 flex justify-center gap-10 text-3xl">
-                <div className="h-10 flex gap-20">
-                    <Link href="mailto:gavinxue050705@gmail.com" className="w-full h-full icon-bounce" target="_blank" rel="noopener noreferrer">
-                        <lottie-player src="/mail-icon.json" autoplay></lottie-player>
-                    </Link>
-                    <Link href="https://www.linkedin.com/in/gavin-xue/" className="w-full h-full icon-bounce" target="_blank" rel="noopener noreferrer">
-                        <lottie-player src="/linkedin-icon.json" autoplay></lottie-player>
-                    </Link>
-                    <Link href="https://github.com/gavxue" className="w-full h-full icon-bounce" target="_blank" rel="noopener noreferrer">
-                        <lottie-player src="/github-icon.json" autoplay></lottie-player>
-                    </Link>
-                </div>
-            </div>
-        </header >
-    )
+  return (
+    <header className="flex flex-col text-center my-60">
+      {/* <h1 className="text-8xl">Gavin Xue</h1> */}
+      <Link href="" className="name hvr-icon-bob hvr-icon">
+        <Lottie animationData={name} play loop={false} />
+      </Link>
+      <div className="h-10 mt-10 flex justify-center gap-10 text-3xl">
+        <div className="h-10 flex gap-20">
+          <Link
+            href="mailto:gavinxue050705@gmail.com"
+            className="w-full h-full icon-bounce"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Lottie
+              animationData={mailIcon}
+              play
+              loop={false}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/gavin-xue/"
+            className="w-full h-full icon-bounce"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Lottie
+              animationData={linkedinIcon}
+              play
+              loop={false}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </Link>
+          <Link
+            href="https://github.com/gavxue"
+            className="w-full h-full icon-bounce"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Lottie
+              animationData={githubIcon}
+              play
+              loop={false}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
