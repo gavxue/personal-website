935cbc7ef6e44efa946b68d95c0e0c5e915b30a2
Gavin Xue <gavinxue050705@gmail.com>
2025-05-01 18:40:34 -0400

    updated experience and projects

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,40 +1,84 @@
 export default function Experience() {
-    return (
-        <section className="text-center my-20" data-aos="fade-up">
-            <h2 className="text-4xl font-bold">Experience</h2>
-            <div className="hvr-grow">
-                <div className="flex flex-col md:flex-row justify-between text-left my-5" data-aos="fade-up">
-                    <div className="">
-                        <p className="font-bold">Information Technology Support Assistant</p>
-                        <p>University of Waterloo - Department of Civil and Environmental Engineering</p>
-                    </div>
-                    <div className="my-auto">
-                        <p>May 2024 - Aug 2024</p>
-                    </div>
-                </div>
-            </div>
-            <div className="hvr-grow">
-                <div className="flex flex-col md:flex-row justify-between text-left my-5" data-aos="fade-up">
-                    <div className="">
-                        <p className="font-bold">Web Developer</p>
-                        <p>UW Mathematics Society</p>
-                    </div>
-                    <div className="my-auto">
-                        <p>May 2024 - Aug 2024</p>
-                    </div>
-                </div>
-            </div>
-            <div className="hvr-grow">
-                <div className="flex flex-col md:flex-row justify-between text-left my-5" data-aos="fade-up">
-                    <div className="">
-                        <p className="font-bold">Frontend Developer</p>
-                        <p>UW Google Developer Student Clubs</p>
-                    </div>
-                    <div className="my-auto">
-                        <p>Jan 2024 - Present</p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className="text-center my-20" data-aos="fade-up">
+      <h2 className="text-4xl font-bold">Experience</h2>
+      <div className="hvr-grow">
+        <div
+          className="flex flex-col md:flex-row justify-between text-left my-5"
+          data-aos="fade-up"
+        >
+          <div className="">
+            <p className="font-bold">Software Developer</p>
+            <p>Environment and Climate Change Canada</p>
+          </div>
+          <div className="my-auto">
+            <p>Jan 2025 - Apr 2025</p>
+          </div>
+        </div>
+      </div>
+      <div className="hvr-grow">
+        <div
+          className="flex flex-col md:flex-row justify-between text-left my-5"
+          data-aos="fade-up"
+        >
+          <div className="">
+            <p className="font-bold">Software Developer</p>
+            <p>Develop for Good</p>
+          </div>
+          <div className="my-auto">
+            <p>Oct 2024 - Feb 2025</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="hvr-grow">
+        <div
+          className="flex flex-col md:flex-row justify-between text-left my-5"
+          data-aos="fade-up"
+        >
+          <div className="">
+            <p className="font-bold">Web Developer</p>
+            <p>UW Mathematics Society</p>
+          </div>
+          <div className="my-auto">
+            <p>May 2024 - Aug 2024</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="hvr-grow">
+        <div
+          className="flex flex-col md:flex-row justify-between text-left my-5"
+          data-aos="fade-up"
+        >
+          <div className="">
+            <p className="font-bold">
+              Information Technology Support Assistant
+            </p>
+            <p>
+              University of Waterloo
+            </p>
+          </div>
+          <div className="my-auto">
+            <p>May 2024 - Aug 2024</p>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="hvr-grow">
+        <div
+          className="flex flex-col md:flex-row justify-between text-left my-5"
+          data-aos="fade-up"
+        >
+          <div className="">
+            <p className="font-bold">Frontend Developer</p>
+            <p>UW Google Developer Student Clubs</p>
+          </div>
+          <div className="my-auto">
+            <p>Jan 2024 - Present</p>
+          </div>
+        </div>
+      </div> */}
+    </section>
+  );
 }
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,39 +1,67 @@
-
 import Link from "next/link";
 import { FiSearch } from "react-icons/fi";
+import { HiOutlineSparkles } from "react-icons/hi2";
 import { PiPlanetFill } from "react-icons/pi";
 import { TbFaceId } from "react-icons/tb";
 
 export default function Projects() {
-    return (
-        <section className="my-20" data-aos="fade-up">
-            <h2 className="text-center text-4xl font-bold">Projects</h2>
-            <div className="grid grid-cols-2 sm:grid-cols-3 my-5 gap-5">
-                <Link href="https://github.com/gavxue/broken-link-checker" className="hvr-grow" target="_blank" rel="noopener noreferrer">
-                    <div data-aos="fade-up" data-aos-delay="100">
-                        <FiSearch className="text-4xl" />
-                        <p className="font-bold">Broken Link Checker</p>
-                        <p className="italic">Flask, BeautifulSoup4, Heroku</p>
-                        <p>A web-scraper that checks WCMS 3 sites for broken links.</p>
-                    </div>
-                </Link>
-                <Link href="https://github.com/gavxue/red-snap" className="hvr-grow" target="_blank" rel="noopener noreferrer">
-                    <div data-aos="fade-up" data-aos-delay="300">
-                        <PiPlanetFill className="text-4xl" />
-                        <p className="font-bold">Red Snap</p>
-                        <p className="italic">React, NASA Open API</p>
-                        <p>View images taken by NASA Mars rovers.</p>
-                    </div>
-                </Link>
-                <Link href="https://github.com/gavxue/face-recognition" className="hvr-grow" target="_blank" rel="noopener noreferrer">
-                    <div data-aos="fade-up" data-aos-delay="500">
-                        <TbFaceId className="text-4xl" />
-                        <p className="font-bold">Face Detection</p>
-                        <p className="italic">Python, TensorFlow, OpenCV</p>
-                        <p>Detects human faces from a laptop camera.</p>
-                    </div>
-                </Link>
-            </div>
-        </section>
-    )
+  return (
+    <section className="my-20" data-aos="fade-up">
+      <h2 className="text-center text-4xl font-bold">Projects</h2>
+      <div className="grid grid-cols-2 sm:grid-cols-3 my-5 gap-5">
+        <Link
+          href="https://github.com/gavxue/notefuse"
+          className="hvr-grow"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div data-aos="fade-up" data-aos-delay="100">
+            <HiOutlineSparkles className="text-4xl" />
+            <p className="font-bold">notefuse</p>
+            <p className="italic">Next.js, AWS S3, Pinecone</p>
+            <p>An AI-powered chatbot that assists with summarizing notes.</p>
+          </div>
+        </Link>
+        <Link
+          href="https://github.com/gavxue/broken-link-checker"
+          className="hvr-grow"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div data-aos="fade-up" data-aos-delay="300">
+            <FiSearch className="text-4xl" />
+            <p className="font-bold">Broken Link Checker</p>
+            <p className="italic">Flask, BeautifulSoup4, Heroku</p>
+            <p>A web-scraper that checks WCMS 3 sites for broken links.</p>
+          </div>
+        </Link>
+        {/* <Link
+          href="https://github.com/gavxue/red-snap"
+          className="hvr-grow"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div data-aos="fade-up" data-aos-delay="300">
+            <PiPlanetFill className="text-4xl" />
+            <p className="font-bold">Red Snap</p>
+            <p className="italic">React, NASA Open API</p>
+            <p>View images taken by NASA Mars rovers.</p>
+          </div>
+        </Link> */}
+        <Link
+          href="https://github.com/gavxue/face-recognition"
+          className="hvr-grow"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div data-aos="fade-up" data-aos-delay="500">
+            <TbFaceId className="text-4xl" />
+            <p className="font-bold">Face Detection</p>
+            <p className="italic">Python, TensorFlow, OpenCV</p>
+            <p>Detects human faces from a laptop camera.</p>
+          </div>
+        </Link>
+      </div>
+    </section>
+  );
 }
